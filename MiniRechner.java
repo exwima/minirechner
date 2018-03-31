a0cdf168a741513e5898891113284010ae0bfd44
exwima <37637697+exwima@users.noreply.github.com>
2018-03-31 17:37:18 +0200

    Create MiniRechner.java
    kann zwei Zahlen einlesen + addieren

---
+++ MiniRechner.java
@@ -0,0 +1,39 @@
+package zweiZahlen;
+
+import java.util.Scanner;
+
+public class MiniRechner {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Zwei Zahlen einlesen
+        System.out.println("Hallo. Bitte geben Sie Zahl a ein.");
+        int a = scanner.nextInt();
+        System.out.println("Bitte geben Sie Zahl b ein.");
+        int b = scanner.nextInt();
+        
+        // Kontrollausgabe
+        System.out.println("Zahl a ist: " + a);
+        System.out.println("Zahl b ist: " + b);
+
+        // Funktion wählen
+        System.out.println("Bitte wählen Sie eine Funktion: (a) = addieren");
+        char f = scanner.next().charAt(0);
+
+        // Funktion ausführen
+        if (f == 'a') {
+            int erg = a + b;
+            System.out.println("Summe a+b: " + a + " + " + b + " = " + erg);
+        } else {
+            System.out.println("Operation nicht verfügbar.");
+        }
+
+        // fertig
+        System.out.println("Tschüss.");
+        scanner.close();
+
+    }
+
+}
